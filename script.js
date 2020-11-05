dd595757577b062cc1af8e18b8a4205201d218b9
Hemant-sood <hemantsood1999@gmail.com>
2020-11-05 19:19:18 +0530

    First commit which include home page and js page two page left

---
+++ script.js
@@ -0,0 +1,11 @@
+function clickOnCss() {
+    window.location.href = 'css.html';
+}
+
+function clickOnHtml() {
+    window.location.href = 'html.html';
+}
+
+function clickOnJs() {
+    window.location.href = 'js.html';
+}
